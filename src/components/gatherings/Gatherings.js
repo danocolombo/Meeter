e08bd5695cea4b3849b003796f102fdb28ea5222
danocolombo@google.com <GR0mans1212!>
2022-01-19 06:20:00 -0500

    scroll to top on reload for gatherings page

--- src/components/gatherings/Gatherings.js
+++ src/components/gatherings/Gatherings.js
@@ -15,44 +15,60 @@ const Gatherings = ({
 }) => {
     useEffect(() => {
         getGatherings(meeter.active.client);
+        window.scrollTo(0, 0);
     }, []);
     const [presentView, setPresentView] = useState(true);
 
     const handleViewClick = () => {
         setPresentView(!presentView);
-    }
-    
+    };
+
     return auth.loading || loading ? (
         <Spinner />
     ) : (
         <Fragment>
             {presentView === true ? (
                 <>
-                <h2>Current Meetings</h2>
-                <Link key='future' to='/EditGathering/0'>
-                    <div key='one' className='waves-effect waves-light btn'>
-                        <i key='two' className='material-icons left green'>
-                            add_circle_outline
-                        </i>
-                        <span key='three'className='meeterNavTextHighlight'>
-                            {'  '}NEW
-                        </span>
-                    </div>
-                </Link>
+                    <h2>Current Meetings</h2>
+                    <Link key='future' to='/EditGathering/0'>
+                        <div key='one' className='waves-effect waves-light btn'>
+                            <i key='two' className='material-icons left green'>
+                                add_circle_outline
+                            </i>
+                            <span
+                                key='three'
+                                className='meeterNavTextHighlight'
+                            >
+                                {'  '}NEW
+                            </span>
+                        </div>
+                    </Link>
                 </>
-            ) : ( <h2>Past Meetings</h2>)}
-            {presentView === true ? (
-                <div onClick={()=>handleViewClick()}>View past meetings</div>
-            ):(<div onClick={()=>handleViewClick()}>View upcoming meetings</div>)}
-            {presentView === true ? (
-                gatherings.map((gathering) => (
-                    <GatheringItem key={gathering.meetingId} gathering={gathering} presentView={presentView}/>
-                ))
             ) : (
-                hatherings.map((hathering) => (
-                    <GatheringItem key={hathering.meetingId} gathering={hathering} presentView={presentView}/>
-                ))
+                <h2>Past Meetings</h2>
             )}
+            {presentView === true ? (
+                <div onClick={() => handleViewClick()}>View past meetings</div>
+            ) : (
+                <div onClick={() => handleViewClick()}>
+                    View upcoming meetings
+                </div>
+            )}
+            {presentView === true
+                ? gatherings.map((gathering) => (
+                      <GatheringItem
+                          key={gathering.meetingId}
+                          gathering={gathering}
+                          presentView={presentView}
+                      />
+                  ))
+                : hatherings.map((hathering) => (
+                      <GatheringItem
+                          key={hathering.meetingId}
+                          gathering={hathering}
+                          presentView={presentView}
+                      />
+                  ))}
         </Fragment>
     );
 };
