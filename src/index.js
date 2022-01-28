780efaf42c40434976599fea31617ecbd19ec220
Dano Colombo <danocolombo@gmail.com>
2022-01-27 20:53:01 -0500

    ...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Amplify from "aws-amplify";
+
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
